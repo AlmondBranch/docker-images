0a404ac249a68b07b32cfc44ec05806838f988e4
Scott Haney <emailalmondbranch@gmail.com>
2020-05-21 22:26:27 -0400

    Update the readmes for the document database related images and add in a script for setting up the pipeline in elasticsearch

--- documentsclient/documents-client.dockerfile
+++ documentsclient/documents-client.dockerfile
@@ -4,4 +4,4 @@ RUN pip3 install elasticsearch
 RUN pip3 install PyPDF2
 RUN pip3 install fpdf
 
-
+COPY resources /home/
---
+++ elasticsearch/dbsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -XPUT "localhost:9200/_ingest/pipeline/attachment?pretty" -H 'Content-Type: application/json' -d'
+{
+  "description" : "Field for processing file attachments",
+  "processors" : [
+    {
+      "attachment" : {
+        "field" : "data"
+      }
+    }
+  ]
+}
+'
