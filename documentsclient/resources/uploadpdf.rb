e4bf9fade22bddd8e11a80d132256cfce4956ca3
Scott Haney <emailalmondbranch@gmail.com>
2020-05-26 18:55:23 -0400

    For now comment out the delete line in the uploader to avoid an error on a new install

--- documentsclient/resources/uploadpdf.rb
+++ documentsclient/resources/uploadpdf.rb
@@ -6,5 +6,5 @@ client = Elasticsearch::Client.new url: 'http://localhost:9200', log: true
 pdf_text = File.read("pcm_ieee_micro10.pdf", :encoding => 'utf-8')
 pdf_base64 = Base64.encode64(pdf_text)
 
-client.delete index: "pdf", type: "_doc", id: 42
+#client.delete index: "pdf", type: "_doc", id: 42
 client.index index: "pdf", type: "_doc", id: 42, body: {data: pdf_base64} 
